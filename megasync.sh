2890b549fd7dfae223c3fb03bd9ea827675e8d27
VicdeJuan <dejuan.victor@gmail.com>
2015-02-10 12:19:01 +0100

    Mega_sync

---
+++ megasync.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+export MEGA_PWD=$(cat ~/.mega_pwd)
+export MEGA_EMAIL=$(cat ~/.mega_em)
+
+
+local="/home/vicdejuan/Documents/Carrera/.pdf"
+
+repo_dir="/home/vicdejuan/Documents/Carrera/Apuntes"
+remote="Doble\ Grado\ UAM\ \(1\)/Apuntes\ Latex"
+
+echo $remote
+## Sync folder.
+#megasimplesync $local $remote
+
+
+
